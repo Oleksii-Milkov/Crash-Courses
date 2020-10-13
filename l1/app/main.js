16f56fa5b3b915608e10a109826105720280a247
Oleksii-Milkov <mouzer92@gmail.com>
2020-10-13 23:29:16 +0300

    lesson-1

---
+++ l1/app/main.js
@@ -0,0 +1,52 @@
+//Task1
+/* const firstName = prompt("Enter your name:");
+const lastName = prompt("Enter yout last name:");
+alert(`Hello, ${lastName}, ${firstName}.`); */
+
+//Task2
+/* const currentYear = 2020;
+const yearOfBirth = +prompt("Enter your year of birth:");
+alert("Your age is: " + (currentYear - yearOfBirth)); */
+
+//Task3
+/* const length = prompt("Enter side length of num square");
+alert("Perimeter = " + (length * 4) + ", Square area = " + (length * length)); */
+
+//Task4
+/*const p = 3.1415;
+const radius = prompt("Enter radius of your circles");
+const rinsing = (radius ** 2) * p; 
+alert (`Rinsing of your circles = ${rinsing}`);*/
+
+//Task5
+/*const wayInKm = prompt("Please tell me the distance (in km) between the two cities you have chosen");
+const time = prompt("And the time for which you want to get(in hours)");
+const wayInMt = wayInKm * 1000;
+const speed = wayInKm/time;
+alert(`This is the speed you need to have to get from one city to another = ${speed} hours`);*/
+
+//Task6
+/* let num = +prompt("Enter count of your dollars:");
+const UAH = 28.32;
+const EUR = 0.85;
+alert(num + " USD = " + Math.floor(num * UAH * 100) / 100 + " UAH");
+alert(num + " USD = " + Math.floor(num * EUR * 100) / 100 + " EUR"); */
+
+//Task7
+/* const size = +prompt("Enter flash drive size:");
+alert(Math.floor((size * 1024) / 820)); */
+
+//Task8
+/* const wallet = +prompt("Enter your budget:");
+const choсoPrice = +prompt("Enter one chocolate price:");
+let res = Math.floor(wallet / choсoPrice);
+alert("You can buy " + res + " chocolates" + ", your rest " + (wallet / res)); */
+
+//Task9
+/* const num = +prompt("Enter thee digit number:");
+let a, b, c, d;
+a = Math.floor(num / 100);
+b = num - a * 100;
+c = Math.floor(b / 10);
+d = b - c * 10;
+alert(d + "" + c + "" + a); */
